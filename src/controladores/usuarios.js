6441da30b2e11c2e9cace8a2a9b3e9e341ee4bb4
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-10 15:24:42 -0300

    criptografando senha com o banco de dados bcrypt

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,10 +1,14 @@
 const pool = require('../conexao')
+const bcrypt = require('bcrypt')
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body
 
     try {
-        const novoUsuario = await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *', [nome, email, senha])
+        const senhaCriptografada =  await bcrypt.hash(senha, 10)
+
+        
+        const novoUsuario = await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3) returning *', [nome, email, senhaCriptografada])
 
         return res.status(201).json(novoUsuario.rows[0])
 
